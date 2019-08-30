f7f0a1fddec20fc77f9f1b138230da8eabf6b5ee
CA <2101706920@qq.com>
2019-08-30 17:37:54 +0800

    v1

---
+++ 前端练习/前端项目(旧)/网站注册页面/表单标签/1.js
@@ -0,0 +1,25 @@
+function init() {
+	setInterval("changeImg()", 1000);
+	time = setInterval("showAd()", 3000);
+}
+var i = 0
+
+function changeImg() {
+	i++;
+	document.getElementById("img1").src = "img/" + i + ".jpg";
+	if (i == 3) {
+		i = 0;
+	}
+}
+
+function showAd() {
+	var adEle = document.getElementById("img100");
+	adEle.style.display = "block"
+	clearInterval(time)
+	time2 = setInterval("hiddenAd()", 1000)
+}
+
+function hiddenAd() {
+	document.getElementById("img100").style.display = "none"
+	clearInterval(time2)
+}
